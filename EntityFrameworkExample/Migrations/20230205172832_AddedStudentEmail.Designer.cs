8d58478c314d96204e4fa5e2c21ce16d7a0e0189
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-05 09:50:33 -0800

    Added EmailAddress to Student class and updated DB with migration

--- EntityFrameworkExample/Migrations/20230205172832_AddedStudentEmail.Designer.cs
+++ EntityFrameworkExample/Migrations/20230205172832_AddedStudentEmail.Designer.cs
@@ -4,6 +4,7 @@ using EntityFrameworkExample;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkExample.Migrations
 {
     [DbContext(typeof(StudentContext))]
-    partial class StudentContextModelSnapshot : ModelSnapshot
+    [Migration("20230205172832_AddedStudentEmail")]
+    partial class AddedStudentEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EntityFrameworkExample/Student.cs
+++ EntityFrameworkExample/Student.cs
@@ -13,5 +13,7 @@ namespace EntityFrameworkExample
         public int Id { get; set; }
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
     }
 }
