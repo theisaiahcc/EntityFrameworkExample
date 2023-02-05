7200321d9d258c42f5fe9d97b8f5bdf5b6beebda
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-05 08:53:12 -0800

    Created a student model to track in the database

---
+++ EntityFrameworkExample/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public class Student
+    {
+        [Key] // Tell EF Core to make this a Primary Key
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
